c129886b01558e41a51faaca577966d917ecaba4
dineshr93 <dineshr93@gmail.com>
2014-05-05 07:27:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Owl::Application.config.secret_key_base = '5c8d9830779aaa9b1ab2548f202778df20651d795304fcef7acd3769e63ad0744ed1021694495660fc74aa771d461d92ded6fd95303e67108bd00cc6b6b3c07a'
